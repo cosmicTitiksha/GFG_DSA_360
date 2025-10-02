e03b8ee01110a18233930abd060a3fec84e2694e
Titiksha <t02437356@gmail.com>
2025-10-02 13:57:12 +0530

    using loop from range 2 to int(math.sqrt(num)) + 1

--- maths_and_combinatorics/identifying_prime_number.py
+++ maths_and_combinatorics/identifying_prime_number.py
@@ -1,11 +1,28 @@
 # The program is to check if a number is prime or not
+import math
+# num = int(input("Enter the number you want to check: "))
+# lista = []
+# for i in range(2,num):
+#   if num % i == 0:
+#     lista.append(i)
+# if len(lista) == 0:
+#   print("The number is prime.")
+# else:
+#   print("The number is not prime.")
 
-num = int(input("Enter the number you want to check: "))
+num = int(input("Enter the number : "))
 lista = []
-for i in range(2,num):
-  if num % i == 0:
-    lista.append(i)
-if len(lista) == 0:
-  print("The number is prime.")
+def prime(num):
+  for i in range(2, int(math.sqrt(num))+1):
+    if num % i == 0:
+      lista.append(i)
+  return lista
+if num <= 1:
+  print("The number is not prime. Numbers less than or equal to 1 are not prime.")
+
 else:
-  print("The number is not prime.")
+  prime(num)
+  if len(lista) == 0:
+    print("The number is prime.")
+  else:
+    print("The number is not prime.")
